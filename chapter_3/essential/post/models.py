a7f4dfbbacdcb5037c14050808e6f664ad2c750f
kerrrbo <dltnrhks2@gmail.com>
2025-01-23 07:48:15 +0900

    update chapter3

--- chapter_3/essential/post/models.py
+++ chapter_3/essential/post/models.py
@@ -15,10 +15,13 @@ class Post(models.Model):
     
     # 좋아요 필드 생성
     like = models.ManyToManyField("user.CustomUser", related_name="likes", blank=True)
-    
+
 
 class Comment(models.Model):
+    
+    # Post와 일대다 관계 설정
     post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
+    # CustomUser와 일대다 관계 설정
     author = models.ForeignKey("user.CustomUser", on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     created_at = models.TimeField(auto_now_add=True)
